da04c992cccf2691f8b58e9cd5054b26d56abe6d
Dariusz Lubomski <dariusz.lubomski@balticdatascience.com>
2017-06-04 11:15:04 +0200

    added logging

--- get_all_bacteria.py
+++ get_all_bacteria.py
@@ -1,20 +1,24 @@
 from Bio.Blast import NCBIWWW
 from pathlib2 import Path
+import logging
 
 data_in_catalog = "DataIn"
-data_out_catalog = 'DataOut2'
+data_out_catalog = 'DataOut'
+
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
+
 
 data_in = Path('.') / data_in_catalog
 bacteria_paths = [x for x in data_in.iterdir() if x.is_file()]
 
 for microbe_path in bacteria_paths:
-    print("Processing: {0}".format(microbe_path))
+    logging.info("Processing: {0}".format(microbe_path))
 
     with microbe_path.open() as f:
         rna = f.read()
 
         if rna == "":
-            print "EMPTY FILE !!!"
+            logging.warning("EMPTY FILE !!!")
             continue
 
         microbe = microbe_path.stem
@@ -25,16 +29,15 @@ for microbe_path in bacteria_paths:
         path.mkdir(parents=True, exist_ok=True)
 
         result_handle = NCBIWWW.qblast(megablast=True, program="blastn", database="nt", sequence=rna, format_type="XML")
-        with open("{0}/{1}.xml".format(str(path), microbe), "w") as out_handle:
+        with open("{0}/{1}.xml".format(path, microbe), "w") as out_handle:
             out_handle.write(result_handle.read())
             result_handle.close()
-        print("\txml done.")
+        logging.info("\txml done.")
 
         result_handle = NCBIWWW.qblast(megablast=True, program="blastn", database="nt", sequence=rna,
                                        format_type="HTML")
-        with open("{0}/{1}.html".format(str(path), microbe), "w") as out_handle:
+        with open("{0}/{1}.html".format(path, microbe), "w") as out_handle:
             out_handle.write(result_handle.read())
             result_handle.close()
-        print("\thtml done.")
-
-print("Ended.")
+        logging.info("\thtml done.")
+logging.info("Ended.")
